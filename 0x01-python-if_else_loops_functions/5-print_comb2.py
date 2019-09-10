4bbd9d32ee9978f4a0dee65574852815a4b5b298
miguel-dev <miguelacortes@zoho.com>
2019-09-10 23:10:31 +0000

    [Fixes] Prints just one 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for n in range(0, 100):
+for n in range(0, 99):
     print("{:02d}".format(n), end=", ")
-    if n == 99:
-        print("{:d}".format(n))
+print("{:d}".format(99))
