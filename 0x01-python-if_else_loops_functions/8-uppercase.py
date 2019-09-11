afcbc311e187b6ed550072e89600abfd9bb362d1
miguel-dev <miguelacortes@zoho.com>
2019-09-11 00:17:32 +0000

    [Fixes] Prints using string format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-    ch = ''
     for char in str:
-        if ord(char) >= 97 and ord(char) <= 122:
-            ch = chr(ord(char) - 32)
+        ascii_code = ord(char)
+        if ascii_code >= 97 and ascii_code <= 122:
+            character = chr(ascii_code - 32)
         else:
-            ch = char
-        print(ch, end="")
+            character = char
+        print("{}".format(character), end="")
     print()
