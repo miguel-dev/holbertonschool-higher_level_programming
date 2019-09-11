836bcd8749fa8e18b383db4ba6f09e18b6b2ddad
miguel-dev <miguelacortes@zoho.com>
2019-09-11 00:05:22 +0000

    [Fixes] Uses only two print

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
+    ch = ''
     for char in str:
         if ord(char) >= 97 and ord(char) <= 122:
-            print(chr(ord(char) - 32), end="")
+            ch = chr(ord(char) - 32)
         else:
-            print(char, end="")
+            ch = char
+        print(ch, end="")
     print()
