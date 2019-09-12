ef9c326186202500eccf06105bce3af5d4a35379
miguel-dev <miguelacortes@zoho.com>
2019-09-12 22:52:13 +0000

    [Feature] Task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    length = len(argv)
+
+    if (length == 1):
+        print(0)
+    else:
+        addition = 0
+        for i in range(1, length):
+            addition += int(argv[i])
+
+        print(addition)
