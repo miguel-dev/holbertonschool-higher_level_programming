6b42fd0d7a289dbc664ab64b2c9cbe5de8af56cf
miguel-dev <miguelacortes@zoho.com>
2019-09-12 00:45:27 +0000

    [Fixes] Starts from character equivalent to 97 in ASCII

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-for c in reversed(range(96, 123)):
-    if (c % 2 == 0):
-        ascii_code = c
-    else:
-        ascii_code = c - 32
-    print("{}".format(chr(ascii_code)), end="")
+for c in reversed(range(97, 123)):
+    if (c % 2):   #Evaluates if it's even
+        c -= 32
+    print("{}".format(chr(c)), end="")
