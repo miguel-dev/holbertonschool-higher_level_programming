740525f26348b58aaa791631a2bab7db8357a354
miguel-dev <miguelacortes@zoho.com>
2019-09-12 23:20:02 +0000

    [Fixes] PEP8

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-   import hidden_4
-   
-   names = dir(hidden_4)[8:]
-   for n in names:
+    import hidden_4
+
+    names = dir(hidden_4)[8:]
+    for n in names:
         print(n)
