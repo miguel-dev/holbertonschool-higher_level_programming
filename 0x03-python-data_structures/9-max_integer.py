ac52faa5680acab9ea7e32ac95084301a885d390
miguel-dev <miguelacortes@zoho.com>
2019-09-18 21:44:16 +0000

    [Fixes] Handles negative numbers

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,11 +2,13 @@
 
 
 def max_integer(my_list=[]):
-    maxx = 0
+    maxx = my_list[0]
 
     if not my_list:
         return None
+
     for i in my_list:
         if i > maxx:
             maxx = i
+
     return maxx
