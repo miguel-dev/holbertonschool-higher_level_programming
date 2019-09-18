fa11098440fb0241358c002eb65b7c19efc32a22
miguel-dev <miguelacortes@zoho.com>
2019-09-18 21:11:23 +0000

    [Fixes] If the sentence is empty, returns a tuple with length 0 and first character None

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,9 @@
 
 
 def multiple_returns(sentence):
-    if not sentence or sentence == "":
+    if not sentence:
         return None
+    elif sentence == "":
+        return 0, None
     else:
         return len(sentence), sentence[0]
