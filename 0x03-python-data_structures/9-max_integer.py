7255fe50d61bfdbf78254224093eb926032aaaee
miguel-dev <miguelacortes@zoho.com>
2019-09-18 21:53:37 +0000

    [Fixes] Remove >= for >

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,11 +4,11 @@
 def max_integer(my_list=[]):
     maxx = my_list[0]
 
-    if not my_list:
+    if len(my_list) == 0:
         return None
 
     for i in my_list:
-        if i >= maxx:
+        if i > maxx:
             maxx = i
 
     return maxx
