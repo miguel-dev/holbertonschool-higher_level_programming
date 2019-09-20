cba32184477dda02da5fe319d90173d02b09203c
miguel-dev <miguelacortes@zoho.com>
2019-09-20 14:18:31 +0000

    [Fixes] Deleted unnecessary code

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-
-
 def mutiply_list_map(my_list=[], number=0):
-    new_list = my_list.copy()
-    return list(map(lambda x: x*number, new_list))
+    return list(map(lambda x: x*number, my_list))
