8abf6b98f45d6f02a1ba6cb2034e519bda1d31f5
miguel-dev <miguelacortes@zoho.com>
2019-09-20 01:15:20 +0000

    [Feature] Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,14 +2,15 @@
 
 
 def search_replace(my_list, search, replace):
+    length = len(my_list)
 
     if not my_list:
         return None
 
     new_list = my_list[:]
 
-    for i in my_list:
-        if i == search:
+    for i in range(length):
+        if new_list[i] == search:
             new_list[i] = replace
 
     return new_list
