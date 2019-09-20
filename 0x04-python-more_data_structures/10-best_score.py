e748554fda5c5d12a832636246c9ccecaeb33677
miguel-dev <miguelacortes@zoho.com>
2019-09-20 03:21:20 +0000

    [Fixes] Variable name for max value is now correct

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    max_value = max(a_dictionary.values())
+    for k, v in a_dictionary.items():
+        if v == max_value:
+            return k
