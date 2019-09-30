f55c4276a5a02e14c597ab195588978dd494b92f
miguel-dev <miguelacortes@zoho.com>
2019-09-30 20:48:16 +0000

    [Fixes] Added try except clauses

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,12 @@
 def safe_print_list(my_list=[], x=0):
     printed = 0
-    for element in my_list:
-        if printed < x:
-            print("{}".format(element), end="")
-            printed = printed + 1
-    print()
+    length = len(my_list)
+    try:
+        for element in range(1, length):
+            if printed < x:
+                print("{}".format(element), end="")
+                printed = printed + 1
+        print()
+    except IndexError:
+        print("List is empty.")
     return printed
