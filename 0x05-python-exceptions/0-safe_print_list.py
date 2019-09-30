b3ea66f204511873d487b797d1d79d2c0749b16b
miguel-dev <miguelacortes@zoho.com>
2019-09-30 21:08:06 +0000

    [Fixes] Added #!/usr/bin/python3 on first line

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     printed = 0
     try:
         for element in range(0, x):
-            if printed <= x:
-                print("{}".format(my_list[element]), end="")
-                printed = printed + 1
+            print("{}".format(my_list[element]), end="")
+            printed = printed + 1
         print()
     except IndexError:
         print()
-        pass
     return printed
