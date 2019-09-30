e6133eb90ab4fefd8a744da1d3f6e8e99c02632d
miguel-dev <miguelacortes@zoho.com>
2019-09-30 22:58:54 +0000

    [Feature] Task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    printed = 0
+    for element in range(0, x):
+        try:
+            print("{:d}".format(my_list[element]), end="")
+            printed += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return printed
