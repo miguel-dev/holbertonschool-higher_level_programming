eb10e0ee9a1ca6ee763b9065ce46a108d23c83d9
miguel-dev <miguelacortes@zoho.com>
2019-10-01 23:44:33 +0000

    [Feature] Replaced type with isinstance to check if the type is an integer

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,7 +4,7 @@ class Square:
 
     def __init__(self, size=0):
         """Initiates fields of Square."""
-        if (type(size) != int):
+        if (not isinstance(size, int)):
             raise TypeError("size must be an integer")
         elif (size < 0):
             raise ValueError("size must be >= 0")
