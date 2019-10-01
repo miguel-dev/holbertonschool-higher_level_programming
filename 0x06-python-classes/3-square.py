48af6c342165f39d58f45d79da908bea463ad4e6
miguel-dev <miguelacortes@zoho.com>
2019-10-01 23:59:57 +0000

    [Feature] Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        """Initiates fields of Square."""
+        if (not isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Returns square area."""
+        return self.__size ** 2
