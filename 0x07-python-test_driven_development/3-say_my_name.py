c34fd9cc9b51dcdc7985baa48a0e4e10e4f1431c
miguel-dev <miguelacortes@zoho.com>
2019-10-04 21:47:33 +0000

    [Feature] Task 2

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+3-say_my_name Module
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints My name is <first name> <last name>"""
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    elif not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
