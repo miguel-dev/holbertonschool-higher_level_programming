08b432c57cd0821a6e3bc280a09a111f32f9ab3e
miguel-dev <miguelacortes@zoho.com>
2019-10-05 01:44:49 +0000

    [Feature] Task 4

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module 5-text_indentation for text indentation
+"""
+
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    text = text.strip()
+
+    text = text.replace(". ", ".\n\n")
+    text = text.replace("? ", "?\n\n")
+    text = text.replace(": ", ":\n\n")
+
+    print(text)
