0e0d3e6fc4fa51dd1b2b4d6af6b2bf3fd9e99f0e
miguel-dev <miguelacortes@zoho.com>
2019-10-07 20:32:46 +0000

    [Feature] Task 2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+Module Rectangle
+"""
+
+
+class Rectangle:
+    """Definition of a rectangle."""
+    def __init__(self, width=0, height=0):
+        """Initializes instance variables."""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Get width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Get height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """Returns rectangle area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Returns rectangle perimeter"""
+        return (self.__width * 2) + (self.__height * 2)
