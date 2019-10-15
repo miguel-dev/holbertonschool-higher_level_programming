a3321995b07c2ad40f94b31a43c4a673daa6c703
miguel-dev <miguelacortes@zoho.com>
2019-10-15 23:06:18 +0000

    [Feature] Task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Module Geometry"""
+
+
+class BaseGeometry:
+    """Definition of BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value"""
+        if not type(value) == int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Definition of Rectangle"""
+    def __init__(self, width, height):
+        """Initializes and validates data"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """User friendly representation of a Rectangle"""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+
+    def area(self):
+        """Calculates the area"""
+        return self.__width * self.__height
