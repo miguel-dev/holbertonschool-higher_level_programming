4b8149474029c8221133b2e1f9e40d775d976af3
miguel-dev <miguelacortes@zoho.com>
2019-10-15 19:36:36 +0000

    [Feature] Task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module Exact Same Object"""
+
+
+def is_same_class(obj, a_class):
+    """Evaluates if an object is exclusively an instance of a class"""
+    return type(obj) == a_class
