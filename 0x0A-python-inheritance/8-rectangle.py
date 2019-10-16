03459352f1796510c9c2958c2c275b18e8c07766
miguel-dev <miguelacortes@zoho.com>
2019-10-16 15:21:45 +0000

    [Fixes] In task seven checks if the type of value is an int

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
-"""Module Geometry"""
+"""Module Rectangle"""
 
 
-class BaseGeometry:
-    """Definition of BaseGeometry"""
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates value"""
-        if not type(value) == int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """Definition of Rectangle"""
     def __init__(self, width, height):
         """Initializes and validates data"""
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
+        self.integer_validator("width", width)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
