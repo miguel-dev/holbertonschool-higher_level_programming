6d3581bb461f0586ecb2ea6d2a2a8c5b1278f3ec
miguel-dev <miguelacortes@zoho.com>
2019-10-17 02:57:49 +0000

    [Fixes] PEP8

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -3,5 +3,6 @@
 
 
 def class_to_json(obj):
-    """Returns the dictionary description for JSON serialization of an object"""
+    """Returns the dictionary description for JSON serialization
+       of an object"""
     return obj.__dict__
