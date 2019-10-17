8c28c355cf977e75a6e0e4a93444feeb0021a7c1
miguel-dev <miguelacortes@zoho.com>
2019-10-17 00:01:57 +0000

    [Fixes] Using dump instead of write and dumps

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -7,4 +7,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """Writes an object to a text file using a JSON representation"""
     with open(filename, mode="w", encoding="utf-8") as f:
-        f.write(json.dumps(my_obj))
+        json.dump(my_obj, f)
