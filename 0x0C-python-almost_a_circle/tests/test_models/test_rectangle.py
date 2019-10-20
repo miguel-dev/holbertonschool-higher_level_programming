23331430c40c47a04b9f2a5bf6071d69e8304b79
miguel-dev <miguelacortes@zoho.com>
2019-10-20 19:26:35 +0000

    [Fixes] Added new tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -3,25 +3,44 @@ from models.rectangle import Base
 from models.rectangle import Rectangle
 
 class TestRectangle(unittest.TestCase):
+
+    def setUp(self):
+        self.rectangle = Rectangle(3, 2, 4, 4)
+
     def test_inheritance(self):
         self.assertTrue(issubclass(Rectangle, Base))
 
-    def test_width_setter(self):
-        rectangle = Rectangle(3, 2)
-        rectangle.width = 5
-        self.assertEqual(rectangle.width, 5)
+    def test_width_validation(self):
+        self.assertRaises(TypeError, self.rectangle.__init__, "3", 2)
+        self.assertRaises(ValueError, self.rectangle.__init__, 0, 4)
 
-    def test_height_setter(self):
-        rectangle = Rectangle(3, 2)
-        rectangle.height = 7
-        self.assertEqual(rectangle.height, 7)
+    def test_width_initialization(self):
+        self.rectangle.width = 5
+        self.assertEqual(self.rectangle.width, 5)
 
-    def test_x_setter(self):
-        rectangle = Rectangle(3, 2, 4, 4)
-        rectangle.x = 1
-        self.assertEqual(rectangle.x, 1)
+    def test_height_validation(self):
+        self.assertRaises(TypeError, self.rectangle.__init__, 4, 5.3)
+        self.assertRaises(ValueError, self.rectangle.__init__, 8, -3)
 
-    def text_y_setter(self):
-        rectangle = Rectangle(3, 2, 4, 4)
-        rectangle.y = 9
-        self.assertEqual(rectangle.y, 9)
+    def test_height_initialization(self):
+        self.rectangle.height = 7
+        self.assertEqual(self.rectangle.height, 7)
+
+    def test_x_validation(self):
+        self.assertRaises(TypeError, self.rectangle.__init__, 6, 3, [3], 9)
+        self.assertRaises(ValueError, self.rectangle.__init__, 2, 1, -8, 0)
+
+    def test_x_initialization(self):
+        self.rectangle.x = 1
+        self.assertEqual(self.rectangle.x, 1)
+
+    def test_y_validation(self):
+        self.assertRaises(TypeError, self.rectangle.__init__, 7, 3, 0, "O")
+        self.assertRaises(ValueError, self.rectangle.__init__, 2, 3, 4, -5)
+
+    def text_y_initialization(self):
+        self.rectangle.y = 9
+        self.assertEqual(self.rectangle.y, 9)
+
+if __name__ == "__main__":
+    unittest.main()
