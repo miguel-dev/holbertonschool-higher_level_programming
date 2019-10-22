aa3c03f764af39a595fdb63bfc214bf769afc80a
miguel-dev <miguelacortes@zoho.com>
2019-10-22 21:07:19 +0000

    [Fixes] Dumps and empty list into string instead of returning [] directly

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,5 +20,5 @@ class Base:
     def to_json_string(list_dictionaries):
         """Returns the JSON representation of a list of dictionaries"""
         if list_dictionaries is None or list_dictionaries == []:
-            return "[]"
+            return json.dumps([])
         return json.dumps(list_dictionaries)
