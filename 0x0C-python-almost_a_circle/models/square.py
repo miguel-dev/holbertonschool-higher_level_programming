449206f2b27cb8ef37b3477c763b1d7db02c96b3
miguel-dev <miguelacortes@zoho.com>
2019-10-22 20:45:22 +0000

    [Fixes] Added some missing docstrings to comment the code

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,6 +7,7 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """Defines a Square"""
     def __init__(self, size, x=0, y=0, id=None):
+        """Initializes a Square"""
         super().__init__(size, size, x, y, id)
 
     @property
@@ -17,10 +18,6 @@ class Square(Rectangle):
     @size.setter
     def size(self, value):
         """Sets size"""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif value <= 0:
-            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
 
@@ -30,6 +27,7 @@ class Square(Rectangle):
                 self.id, self.x, self.y, self.width)
 
     def update(self, *args, **kwargs):
+        """Updates the Square attributes"""
         arg_number = 0
         args_names = ["id", "size", "x", "y"]
         if args and args != []:
