c8f94744f94296a0599c09a6e0975f10e324cb1b
miguel-dev <miguelacortes@zoho.com>
2019-10-22 17:49:42 +0000

    [Feature] Task 15. Dictionary to JSON

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 """Module Base"""
 
+import json
 
 class Base:
     """Defines Base"""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initialization of instance variables"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON representation of a list of dictionaries"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
