e80ea21f2a275296532bd6656cae63ffdea252b0
miguel-dev <miguelacortes@zoho.com>
2019-10-23 01:50:39 +0000

    [Feature] Task 17. JSON string to dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,15 +16,9 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        """Returns the JSON representation of a list of dictionaries"""
-        if list_dictionaries is None or list_dictionaries == []:
-            return json.dumps([])
-        return json.dumps(list_dictionaries)
-
     @classmethod
     def save_to_file(cls, list_objs):
+        """Writes the JSON representation of list_objs to a file"""
         file_name = cls.__name__ + ".json"
         with open(file_name, "w") as file:
             if list_objs is None:
@@ -38,3 +32,17 @@ class Base:
                 if obj != len(list_objs) - 1:
                     file.write(", ")
             file.write("]")
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON representation of a list of dictionaries"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return json.dumps([])
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation"""
+        if json_string is None or json_string == []:
+            return []
+        return json.loads(json_string)
