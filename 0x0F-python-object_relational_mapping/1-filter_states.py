5ec641f192c6cabecd6f299651c99b67cab049ab
miguel-dev <miguelacortes@zoho.com>
2019-12-15 22:35:29 +0000

    [Feature] 1. Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Lists all states with a name starting with N. Database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cu = db.cursor()
+    cu.execute("SELECT * FROM states WHERE name LIKE 'N%';")
+    query_rows = cu.fetchall()
+    for row in query_rows:
+        print(row)
+    cu.close()
+    db.close()
