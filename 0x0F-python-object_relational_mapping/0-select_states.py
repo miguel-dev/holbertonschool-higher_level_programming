74f5daeded95f1dd28276b01996dccdc85e8e51d
miguel-dev <miguelacortes@zoho.com>
2019-12-15 21:42:15 +0000

    [Fixes] PEP8

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import MySQLdb
 import sys
-db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+db = MySQLdb.connect(host="localhost",
+                     user=sys.argv[1],
+                     passwd=sys.argv[2],
+                     db=sys.argv[3])
 cu = db.cursor()
 cu.execute("SELECT * FROM states ORDER BY id")
 query_rows = cu.fetchall()
