b955dce4fc98a6406667602c39720ba1033f66d9
miguel-dev <miguelacortes@zoho.com>
2019-12-17 23:39:03 +0000

    [Fixes] Code is not executed when imported

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -3,17 +3,18 @@
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(host='localhost',
-                     port=3306,
-                     user=sys.argv[1],
-                     passwd=sys.argv[2],
-                     db=sys.argv[3])
-cu = db.cursor()
-cu.execute("""SELECT cities.id, cities.name, states.name
-              FROM cities INNER JOIN states ON cities.state_id = states.id
-              ORDER BY cities.id""")
-query_rows = cu.fetchall()
-for row in query_rows:
-    print(row)
-cu.close()
-db.close()
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cu = db.cursor()
+    cu.execute("""SELECT cities.id, cities.name, states.name
+                  FROM cities INNER JOIN states ON cities.state_id = states.id
+                  ORDER BY cities.id""")
+    query_rows = cu.fetchall()
+    for row in query_rows:
+        print(row)
+    cu.close()
+    db.close()
