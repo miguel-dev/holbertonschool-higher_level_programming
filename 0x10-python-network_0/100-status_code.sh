7cfa868d9a004df694af171f42bdf7e109bbdc2f
miguel-dev <miguelacortes@zoho.com>
2020-01-07 21:22:26 +0000

    [Fixes] Found a way to not use pipes

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script that sends a request to a URL passed as an argument and distrplays only the status code of the response.
-curl -Is "$1" | grep -Po "\b(\d{3})\b" | tr '\n' '\0'
+curl -LIs "$1" -o /dev/null -w '%{http_code}'
