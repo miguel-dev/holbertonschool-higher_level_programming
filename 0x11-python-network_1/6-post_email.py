fdd67d86a010401c2ef57cdf372ac96f35f723db
miguel-dev <miguelacortes@zoho.com>
2020-01-14 16:36:46 +0000

    [Fixes] PEP8

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Accepts a URL and an email address, sends a POST
 request to the passed URL with the email as a parameter,
-        and finally displays the body of the response"""
-        import requests
-        import sys
-        if __name__ == "__main__":
-            email = {'email': sys.argv[2]}
-                r = requests.post(sys.argv[1], data=email)
-        print(r.text)
+and finally displays the body of the response"""
+import requests
+import sys
+if __name__ == "__main__":
+    email = {'email': sys.argv[2]}
+    r = requests.post(sys.argv[1], data=email)
+    print(r.text)
