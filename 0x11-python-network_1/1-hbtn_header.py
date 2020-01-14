368f61030aeec0ff5ad7075681639e86d9388fa2
miguel-dev <miguelacortes@zoho.com>
2020-01-14 00:21:45 +0000

    [Fixes] 1. Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Accepts a URL, sends a request to it and displays the value of the X-Request-Id"""
+"""Accepts a URL, sends a request to it and shows the value of X-Request-Id"""
 import urllib.request
 import sys
 with urllib.request.urlopen(sys.argv[1]) as response:
