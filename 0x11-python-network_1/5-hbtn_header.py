c176b5ce8781ebd2a086e726369c27cb0f1457d9
miguel-dev <miguelacortes@zoho.com>
2020-01-14 16:09:28 +0000

    [Feature] 5. Response header value #1

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Accepts in a URL, sends a request to it and displays the value
+of the variable X_Request-Id in the response header"""
+import requests
+import sys
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
