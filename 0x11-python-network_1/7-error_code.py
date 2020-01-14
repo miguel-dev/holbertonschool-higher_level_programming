e92ed316ba1b41b753fa3311904b53b06b0ac89f
miguel-dev <miguelacortes@zoho.com>
2020-01-14 19:04:53 +0000

    [Fixes] When imported it doesn't execute

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,10 +2,11 @@
 """Accepts a URL, sends a request to it and displays the body of response"""
 import requests
 import sys
-r = requests.get(sys.argv[1])
-sc = r.status_code
-try:
-    r.raise_for_status()
-    print(r.text)
-except:
-    print("Error code: {}".format(r.status_code))
+if __name__ = "__main__":
+    r = requests.get(sys.argv[1])
+    sc = r.status_code
+    try:
+        r.raise_for_status()
+        print(r.text)
+    except:
+        print("Error code: {}".format(r.status_code))
