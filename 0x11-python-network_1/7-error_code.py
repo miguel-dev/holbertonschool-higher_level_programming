9b639c0401ec052a2a628ca1fd0c1bd671165404
miguel-dev <miguelacortes@zoho.com>
2020-01-14 19:06:24 +0000

    [Fixes] __name__ == __main: instead of __name__ = __main__

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,7 +2,7 @@
 """Accepts a URL, sends a request to it and displays the body of response"""
 import requests
 import sys
-if __name__ = "__main__":
+if __name__ == "__main__":
     r = requests.get(sys.argv[1])
     sc = r.status_code
     try:
