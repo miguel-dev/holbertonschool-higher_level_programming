5cb74bbc9de91479f6da629a6192c3606598e2a2
miguel-dev <miguelacortes@zoho.com>
2020-01-14 00:26:47 +0000

    [Fixes] Code is not executed when imported

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,5 +2,6 @@
 """Accepts a URL, sends a request to it and shows the value of X-Request-Id"""
 import urllib.request
 import sys
-with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.headers['X-Request-Id'])
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers['X-Request-Id'])
