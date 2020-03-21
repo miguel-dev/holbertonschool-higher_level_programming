0cfd69767fb766d660109dff51ddcf1d6b0f4810
miguel-dev <miguelacortes@zoho.com>
2020-03-21 17:49:58 -0500

    Delete 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Accepts in a URL, sends a request to it and displays the value
-of the variable X_Request-Id in the response header"""
-import requests
-import sys
-if __name__ == "__main__":
-    r = requests.get(sys.argv[1])
-    print(r.headers.get('X-Request-Id'))
