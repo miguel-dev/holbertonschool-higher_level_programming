db3c0619ca983a0bb97cb556bd53737d22091655
miguel-dev <miguelacortes@zoho.com>
2020-03-21 17:52:56 -0500

    Delete 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const numberArgs = process.argv.length - 2;
-if (numberArgs === 0) {
-  console.log('No argument');
-} else if (numberArgs === 1) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
