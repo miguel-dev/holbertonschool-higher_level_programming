4f766c4477eb62b0714818c24931b7fcf4cbfd50
miguel-dev <miguelacortes@zoho.com>
2020-03-21 17:48:23 -0500

    Delete 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Accepts a URL, sends a request to it and shows the value of X-Request-Id"""
-import urllib.request
-import sys
-if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        print(response.headers['X-Request-Id'])
