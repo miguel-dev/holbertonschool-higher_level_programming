64fff796239ecc368f18f4e4d94aeb5242f0a4e0
miguel-dev <miguelacortes@zoho.com>
2020-03-21 17:49:38 -0500

    Delete 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
-import requests
-response = requests.get("https://intranet.hbtn.io/status")
-print("Body response:")
-print("\t- type: {}".format(type(response.text)))
-print("\t- content: {}". format(response.text))
