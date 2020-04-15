3dc9050ec49a8bd05c410d2fc13e658b98d4c0af
miguel-dev <miguelacortes@zoho.com>
2020-04-15 20:09:05 +0000

    [Fixes] Checks if it's encoded on UTF-8 before printing if it's encoded or not

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,8 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         r = response.read()
         print("Body response:")
-        print(" - type: {}".format(type(r)))
-        print(" - content: {}".format(r))
-        print(" - utf8 content: {}".format(r.decode('utf-8')))
+        print("    - type: {}".format(type(r)))
+        print("    - content: {}".format(r))
+        
+        if r.decode('utf-8'):
+            print("    - utf8 content: {}".format(r.decode('utf-8')))
