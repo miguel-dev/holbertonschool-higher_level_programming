38ea20c6c70ab810876ae68d8704317ae0fafc72
miguel-dev <miguelacortes@zoho.com>
2020-04-15 20:20:06 +0000

    [Fixes] Format uses /t for tabulation per instructions

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,8 +7,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         r = response.read()
         print("Body response:")
-        print("    - type: {}".format(type(r)))
-        print("    - content: {}".format(r))
-        
-        if r.decode('utf-8'):
-            print("    - utf8 content: {}".format(r.decode('utf-8')))
+        print("\t- type: {}".format(type(r)))
+        print("\t- content: {}".format(r))
+        print("\t- utf8 content: {}".format(r.decode('utf-8')))
