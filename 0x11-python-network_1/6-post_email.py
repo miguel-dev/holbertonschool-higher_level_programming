1cf836f2f6d69c208ae82b4269c79e711db993f2
miguel-dev <miguelacortes@zoho.com>
2020-04-16 03:00:37 +0000

    [Fixes] PEP8

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,5 +7,5 @@ displays the body of the response"""
 if __name__ == "__main__":
     import requests
     import sys
-    r = requests.post(sys.argv[1], data = {"email": sys.argv[2]})
+    r = requests.post(sys.argv[1], data={"email": sys.argv[2]})
     print(r.text)
