c0d599b754327283d25212e8d03cf6a98bd83b10
miguel-dev <miguelacortes@zoho.com>
2020-04-16 20:24:18 +0000

    [Feature] 8. Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Get a letter and send a POST request to
+http://0.0.0.0:5000/search_user with the
+letter as a parameter"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    letter = ""
+    if len(sys.argv) >= 2:
+        letter = sys.argv[1]
+
+    url = "http://0.0.0.0:5000/search_user"
+    r = requests.post(url, data={"q": letter})
+    try:
+        j = r.json()
+        if j == {}:
+            print("No result")
+        else:
+            print("[{:d}] {:s}".format(j.get("id"), j.get("name")))
+    except:
+        print("Not a valid JSON")
