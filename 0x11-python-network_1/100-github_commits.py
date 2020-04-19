2df685ba655f47263717616b2887907e06ff3e6b
miguel-dev <miguelacortes@zoho.com>
2020-04-19 03:12:15 +0000

    [Fixes] Minor presentation improvements for clarity

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,16 +9,15 @@ if __name__ == "__main__":
                                                               sys.argv[1])
     r = requests.get(url)
     list_res = r.json()
-    dict_sha = {}
-    dict_name = {}
     r = 10
+
     if len(list_res) < 10:
         r = len(list_res)
 
     for i in range(r):
-        dict_name = list_res[i].get("commit")
-        if (dict_name):
-            dict_name = dict_name.get("author")
-            if (dict_name):
-                dict_name = dict_name.get("name")
+        dict_commit = list_res[i].get("commit")
+        if (dict_commit):
+            dict_author = dict_commit.get("author")
+        if (dict_author):
+            dict_name = dict_author.get("name")
         print("{}: {}".format(list_res[i].get("sha"), dict_name))
