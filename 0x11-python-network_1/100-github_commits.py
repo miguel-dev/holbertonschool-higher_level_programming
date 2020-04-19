6248e21e4046e94178e90c055f2183eafc166a9a
miguel-dev <miguelacortes@zoho.com>
2020-04-19 02:13:24 +0000

    [Fixes] PEP8

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     r = 10
     if len(list_res) < 10:
         r = len(list_res)
-        
+
     for i in range(r):
         dict_name = list_res[i].get("commit")
         if (dict_name):
