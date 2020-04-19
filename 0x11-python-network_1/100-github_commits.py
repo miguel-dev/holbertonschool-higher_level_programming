f0c3396345e6a83c54e7a7e76fee5723cee4e4e5
miguel-dev <miguelacortes@zoho.com>
2020-04-19 02:11:41 +0000

    [Fixes] Considered the case where the lenght of the list is less than 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,7 +11,11 @@ if __name__ == "__main__":
     list_res = r.json()
     dict_sha = {}
     dict_name = {}
-    for i in range(10):
+    r = 10
+    if len(list_res) < 10:
+        r = len(list_res)
+        
+    for i in range(r):
         dict_name = list_res[i].get("commit")
         if (dict_name):
             dict_name = dict_name.get("author")
