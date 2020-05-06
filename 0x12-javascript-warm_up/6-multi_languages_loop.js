3f101892f38689542bc214b961e08a5153ab853a
miguel-dev <miguelacortes@zoho.com>
2020-05-06 16:34:03 +0000

    [Fixes] Can print any number of lines, instead of only 3

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -4,6 +4,6 @@ const secondLine = 'Python is cool';
 const thirdLine = 'Javascript is amazing';
 const lines = [firstLine, secondLine, thirdLine];
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < lines.length; i++) {
   console.log(lines[i]);
 }
