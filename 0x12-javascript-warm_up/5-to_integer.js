53890182def1cc4339c3d4a490e72cefa194b76d
miguel-dev <miguelacortes@zoho.com>
2020-05-06 01:49:30 +0000

    [Fixes] Removed a space on the string on console log, since it inserts a space automatically to separate parameters

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const num = parseInt(process.argv[2]);
 if (isNaN(num)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ', parseInt(process.argv[2]));
+  console.log('My number:', parseInt(process.argv[2]));
 }
