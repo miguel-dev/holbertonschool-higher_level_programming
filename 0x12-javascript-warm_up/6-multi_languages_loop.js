dcabdc92e9289cae6926b48d40fc666b9db8a600
miguel-dev <miguelacortes@zoho.com>
2020-05-06 02:06:33 +0000

    [Feature] 6. Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'Javascript is amazing';
+const lines = [firstLine, secondLine, thirdLine];
+
+for (let i = 0; i < 3; i++) {
+  console.log(lines[i]);
+}
