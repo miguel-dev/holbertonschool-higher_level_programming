4e0dc5c7e48eb171fdab098d6164d63fe514ac08
miguel-dev <miguelacortes@zoho.com>
2020-05-14 02:03:15 +0000

    [Fixes] Checks if json converted to object has count 0 and if so prints 0 and does nothing else

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,18 +6,19 @@ request(url, function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
-    const films = JSON.parse(body).results;
-    if (films.count === 0) {
+    const r = JSON.parse(body);
+    if (r.count === 0) {
       console.log(0);
-      break;
-    }
-    let num = 0;
-    for (const film of films) {
-      const characters = film.characters;
-      for (const character of characters) {
-        if (character === 'https://swapi-api.hbtn.io/api/people/18/') {
-          num++;
-          break;
+    } else {
+      const films = r.results;
+      let num = 0;
+      for (const film of films) {
+        const characters = film.characters;
+        for (const character of characters) {
+          if (character === 'https://swapi-api.hbtn.io/api/people/18/') {
+            num++;
+            break;
+          }
         }
       }
       console.log(num);
