8413f938f721f240516179c1625db2d692a876c8
miguel-dev <miguelacortes@zoho.com>
2020-05-14 01:55:18 +0000

    [Fixes] If count is 0 returns 0 and breaks the else

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,18 +6,18 @@ request(url, function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
-    if (!body) {
+    const films = JSON.parse(body).results;
+    if (films.count === 0) {
       console.log(0);
-    } else {
-      const films = JSON.parse(body).results;
-      let num = 0;
-      for (const film of films) {
-        const characters = film.characters;
-        for (const character of characters) {
-          if (character === 'https://swapi-api.hbtn.io/api/people/18/') {
-            num++;
-            break;
-          }
+      break;
+    }
+    let num = 0;
+    for (const film of films) {
+      const characters = film.characters;
+      for (const character of characters) {
+        if (character === 'https://swapi-api.hbtn.io/api/people/18/') {
+          num++;
+          break;
         }
       }
       console.log(num);
