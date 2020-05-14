c93393b852a8a91e78425ec5a6a6d1a6c1ca6b4f
miguel-dev <miguelacortes@zoho.com>
2020-05-14 01:43:15 +0000

    [Fixes] Code refactor

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,19 +7,20 @@ request(url, function (error, response, body) {
     console.error(error);
   } else {
     if (!body) {
-      return 0;
-    }
-    const films = JSON.parse(body).results;
-    let num = 0;
-    for (const film of films) {
-      const characters = film.characters;
-      for (const character of characters) {
-        if (character === 'https://swapi-api.hbtn.io/api/people/18/') {
-          num++;
-          break;
+      console.log(0);
+    } else {
+      const films = JSON.parse(body).results;
+      let num = 0;
+      for (const film of films) {
+        const characters = film.characters;
+        for (const character of characters) {
+          if (character === 'https://swapi-api.hbtn.io/api/people/18/') {
+            num++;
+            break;
+          }
         }
       }
+      console.log(num);
     }
-    console.log(num);
   }
 });
