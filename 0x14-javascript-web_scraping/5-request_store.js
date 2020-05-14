309f75501b14ef450d6af4733e76dd0b6b99a692
miguel-dev <miguelacortes@zoho.com>
2020-05-14 04:54:18 +0000

    [Fixes] Removed variable wpContents, making code simpler

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,13 +4,11 @@ const fs = require('fs');
 
 const args = process.argv.slice(2);
 
-let wpContents;
 request(args[0], function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
-    wpContents = body;
-    fs.writeFile(args[1], wpContents, 'utf-8', (err) => {
+    fs.writeFile(args[1], body, 'utf-8', (err) => {
       if (err) throw err;
     });
   }
