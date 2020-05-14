2fc21a57615fa30a0ba08d8ae3109c636dd0f5a4
miguel-dev <miguelacortes@zoho.com>
2020-05-14 03:01:15 +0000

    [Fixes] console.log(error) instead of console.error(error)

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,7 @@ const request = require('request');
 const url = process.argv[2];
 request(url, function (error, response, body) {
   if (error) {
-    console.error(error);
+    console.log(error);
   } else {
     const films = JSON.parse(body).results;
     let num = 0;
