c54025bc8ad2eddabdb6572f3833a7934021fa3c
miguel-dev <miguelacortes@zoho.com>
2020-05-14 01:12:34 +0000

    [Feature] 4. Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = 'https://swapi-api.hbtn.io/api/films/';
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const films = JSON.parse(body).results;
+    let num = 0;
+    for (const film of films) {
+      const characters = film.characters;
+      for (const character of characters) {
+        if (character === 'https://swapi-api.hbtn.io/api/people/18/') {
+          num++;
+          break;
+        }
+      }
+    }
+    console.log(num);
+  }
+});
