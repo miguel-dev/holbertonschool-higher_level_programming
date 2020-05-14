828edd35931c33adddb6f0d411a0a60775425287
miguel-dev <miguelacortes@zoho.com>
2020-05-14 01:25:45 +0000

    [Fixes] Receives argument url

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 
-const url = 'https://swapi-api.hbtn.io/api/films/';
+const url = process.argv[2];
 request(url, function (error, response, body) {
   if (error) {
     console.error(error);
