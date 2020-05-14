0445f59b7e01bf3de7d68cbc8894806edf80ce13
miguel-dev <miguelacortes@zoho.com>
2020-05-14 02:48:51 +0000

    [Fixes] Remove break statement

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,22 +6,16 @@ request(url, function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
-    const r = JSON.parse(body);
-    if (r.count === 0) {
-      console.log(0);
-    } else {
-      const films = r.results;
-      let num = 0;
-      for (const film of films) {
-        const characters = film.characters;
-        for (const character of characters) {
-          if (character === 'https://swapi-api.hbtn.io/api/people/18/') {
-            num++;
-            break;
-          }
+    const films = JSON.parse(body).results;
+    let num = 0;
+    for (const film of films) {
+      const characters = film.characters;
+      for (const character of characters) {
+        if (character === 'https://swapi-api.hbtn.io/api/people/18/') {
+          num++;
         }
       }
-      console.log(num);
     }
+    console.log(num);
   }
 });
